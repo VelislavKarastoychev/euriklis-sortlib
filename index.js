3396d679663bd2e423e73657e95d7003ff0fa3fb
VelislavKarastoychev <exel_mmm@abv.bg>
2021-03-29 16:00:32 +0300

    Main file of the package.

---
+++ index.js
@@ -0,0 +1,8 @@
+const validator = require('@euriklis/validator')
+const message = require('@euriklis/message')
+new message().bold().italic().underline()
+    .set_color_yellow().append('Euriklis sortlib package was run successfully...\n')
+    .reset().set_color_cyan().append('This package is a collection of array sorting algorithms ')
+    .append('that are implemented in javascript. The most interesting and useful ')
+    .append('algorithms for sorting that you may use from the package are the merge sort and quick sort algorithms.')
+    .reset().log()
