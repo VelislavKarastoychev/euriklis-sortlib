371efd83f22c9da608d4cb0866afda9fb0460645
VelislavKarastoychev <exel_mmm@abv.bg>
2021-06-03 20:43:19 +0300

    error message for the array parameter in the find worst elements static method.

---
+++ src/Errors/IncorrectArrayParameterInFindWorstElements.js
@@ -0,0 +1,11 @@
+'use strict'
+const message = require('@euriklis/message')
+const texts = require('./errorTexts')
+module.exports = () => {
+    const error_message = new message().bold().italic().underline()
+        .set_color_yellow().append(texts.ErrorText).reset()
+        .set_color_red().append_not_check_mark().append_white_space()
+        .set_color_cyan().append(texts.IncorrectArrayParameterInFindWorstElements)
+        .reset().text
+    throw new Error(error_message)
+}
