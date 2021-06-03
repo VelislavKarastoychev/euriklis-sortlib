885fbd7e60e158fc8c0bb9fef254f577f346b42e
VelislavKarastoychev <exel_mmm@abv.bg>
2021-06-03 14:49:04 +0300

    Text container for the errors

--- src/Errors/errorTexts.js
+++ src/Errors/errorTexts.js
@@ -7,6 +7,15 @@ const IncorrectArgumentInIndexSetter = 'Incorrect argument in the index setter. 
 const IncorrectStatusInSetter = 'Incorrect status in the setter method. Note that the status argument of the method has to be a string with possible values "sorted" and "unsorted".'
 const IncorrectArrayParameterInBucketSort = 'Incorrect array parameter in the bucket_sort method. Note that this method requires the array to consists only from number elements.'
 const IncorrectParameterInGRA = 'Incorrect parameter in the generate random array static method. Note that the parameter n has to be an integer.'
+const IncorrectArrayParameterInFindBestElements = 'Incorrect array parameter in the find_best_elements static method. The array has to be number or string array.'
+const IncorrectArrayParameterInFindWorstElements = 'Incorrect array parameter in the find_worst_elements static method of the SortLib package. To be valid, the array has to be a number or string array.'
+const IncorrectArrayParameterInSortObjectArray = 'Incorrect array parameter in sort_object_array_by_property static method. Note that this array has to include only object type elements.'
+const IncorrectPropertyInSortObjectArray = 'Incorrect property parameter in the sort_object_array_by_property static method. Note that the property has to be a string or string array. If the property/es does not exist in any object element of the array, this element will be omitted.'
+const IncorrectArrayParameterInFindBestInObjectArray = 'Incorrect array parameter in the find_best_in_object_array_by_property static method. Note that this parameter has to be an array, each element of which is an object.'
+const IncorrectPropertyParameterInFindBestInObjectArray = 'Incorrect property parameter in find_best_in_object_array_by_property static method. Note that this parameter has to be an array with string elements or a string value.'
+const IncorrectArrayParameterInFindWorstInObjectArray = 'Incorrect array parameter in find_worst_for_object_array_by_property static method. Note that this parameter has to be an array, each of the elements of which is an object.'
+const IncorrectPropertyParameterInFindWorstInObjectArray = 'Incorrect property parameter in find_worst_for_object_array_by_property static method. Note that this parameter has to be an array, each of the elements of which is a string or a string array.'
+const IncorrectCountParameterInFindWorstInObjectArray = 'Incorrect count parameter in find_worst_for_object_array_by_property static method. Note that this parameter has to be an integer number of a floating point number in the open range (0, 1).'
 module.exports = {
     ErrorText,
     IncorrectArrayDeclarationInSortLibConstructor,
@@ -16,5 +25,14 @@ module.exports = {
     IncorrectStatusInSetter,      
     IncorrectArrayParameterInBucketSort,   
     IncorrectParameterInGRA,
+    IncorrectArrayParameterInFindBestElements,
+    IncorrectArrayParameterInFindWorstElements,
+    IncorrectArrayParameterInSortObjectArray,
+    IncorrectPropertyInSortObjectArray,
+    IncorrectArrayParameterInFindBestInObjectArray,
+    IncorrectPropertyParameterInFindBestInObjectArray,
+    IncorrectArrayParameterInFindWorstInObjectArray,
+    IncorrectPropertyParameterInFindWorstInObjectArray,
+    IncorrectCountParameterInFindWorstInObjectArray,
 
 }
