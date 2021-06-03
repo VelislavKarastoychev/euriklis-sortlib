776c02864e6a10e61b3e1de690062331ce354b10
VelislavKarastoychev <exel_mmm@abv.bg>
2021-06-03 20:28:53 +0300

    error message for the count parameter for the find worst in object array by property method.

---
+++ src/Errors/IncorrectCountParameterInFindWorstInObjectArray.js
@@ -0,0 +1,10 @@
+'use strict'
+const message = require('@euriklis/message')
+const texts = require('./errorTexts')
+module.exports = () => {
+    let error_message = new message().bold().italic().underline()
+        .set_color_yellow().append(texts.ErrorText).reset().set_color_red()
+        .append_not_check_mark().append_white_space().set_color_cyan()
+        .append(texts.IncorrectCountParameterInFindWorstInObjectArray).reset().text
+    throw new Error(error_message)
+}
