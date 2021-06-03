c21dba5251f017cdb7fceeee373606f4f13aee07
VelislavKarastoychev <exel_mmm@abv.bg>
2021-06-03 22:38:12 +0300

    warning messages container

--- src/Warnings/index.js
+++ src/Warnings/index.js
@@ -4,5 +4,9 @@ module.exports = {
     IncorrectAlgorithmDeclaration : require('./IncorrectAlgorithmDeclaration'),
     IncorrectSortModeDeclaration : require('./IncorrectSortModeDeclaration'),
     IncorrectAlgorithmInAlgorithmSetter : require('./IncorrectAlgorithmInAlgorithmSetter'),
-     
+    IncorrectCountParameterInFindBestElements : require('./IncorrectCountParameterInFindBestElements'),
+    IncorrectCountParameterInFindWorstElements : require('./IncorrectCountParameterInFindWorstElements'),
+    IncorrectOrUndefinedModeParameterInSortObjectArray : require('./IncorrectOrUndefinedModeParameterInSortObjectArray'),
+    IncorrectOrUndefinedAlgorithmParameterInSortObjectArray : require('./IncorrectOrUndefinedAlgorithmParameterInSortObjectArray'),
+    
 }
