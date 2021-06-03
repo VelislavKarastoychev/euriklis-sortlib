00db69f2b1ab0e0c62e04cbcc3e07c1f24149ea0
VelislavKarastoychev <exel_mmm@abv.bg>
2021-06-03 14:48:20 +0300

    Errors container file

--- src/Errors/index.js
+++ src/Errors/index.js
@@ -7,6 +7,15 @@ module.exports = {
     IncorrectStatusInSetter: require('./IncorrectStatusInSetter'),
     IncorrectStatusParameterInBucketSort: require('./IncorrectArrayParameterInBucketSort'),
     IncorrectParameterInGRA: require('./IncorrectParameterInGRA'),
-
+    IncorrectArrayParameterInFindBestElements : require('./IncorrectArrayParameterInFindBestElements'),
+    IncorrectArrayParameterInFindWortsElements : require('./IncorrectArrayParameterInFindWorstElements'),
+    IncorrectArrayParameterInSortObjectArray : require('./IncorrectArrayParameterInSortObjectArray'),
+    IncorrectPropertyInSortObjectArray : require('./IncorrectPropertyInSortObjectArray'),
+    IncorrectArrayParameterInFindBestInObjectArray : require('./IncorrectArrayParameterInFindBestInObjectArray'),
+    IncorrectPropertyParameterInFindBestInObjectArray : require('./IncorrectPropertyParameterInFindBestInObjectArray'),
+    IncorrectArrayParameterInFindWorstInObjectArray : require('./IncorrectArrayParameterInFindWorstInObjectArray'),
+    IncorrectPropertyParameterInFindWorstInObjectArray : require('./IncorrectPropertyParameterInFindWorstInObjectArray'),
+    IncorrectCountParameterInFindWorstInObjectArray : require('./IncorrectCountParameterInFindWorstInObjectArray'),
+    
 
 }
