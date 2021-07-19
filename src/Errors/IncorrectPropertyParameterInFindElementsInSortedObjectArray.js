b64e26288ae236f2748aaf72820417a7f4e96050
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-19 14:54:50 +0300

    Error message for the non string array structure of the property parameter of the find elements in sorted object array method.

---
+++ src/Errors/IncorrectPropertyParameterInFindElementsInSortedObjectArray.js
@@ -0,0 +1,15 @@
+'use strict';
+const texts = require('./errorTexts');
+const message = require('@euriklis/message');
+module.exports = () => {
+    const error = new Error();
+    const error_name = new message().bold().italic().underline()
+        .set_color_yellow().append(texts.ErrorText).reset().text;
+    const error_message = new message().set_color_red().append_not_check_mark()
+        .append_white_space().set_color_blue()
+        .append(texts.IncorrectPropertyParameterInFindElementsInSortedObjectArray)
+        .reset().text;
+    error.name = error_name;
+    error.message = error_message;
+    throw error;
+}
