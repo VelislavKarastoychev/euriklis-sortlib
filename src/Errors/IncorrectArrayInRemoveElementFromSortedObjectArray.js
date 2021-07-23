3832d214e569d475c64338fbe8134ed50d38130f
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-23 18:03:56 +0300

    Error message for incorrect array in the remove element from sorted by given property array.

---
+++ src/Errors/IncorrectArrayInRemoveElementFromSortedObjectArray.js
@@ -0,0 +1,12 @@
+'use strict';
+const texts = require('./errorTexts');
+const message = require('@euriklis/message');
+module.exports = () => {
+    const error = new Error();
+    error.name = new message().bold().italic()
+        .underline().set_color_yellow().append(texts.ErrorText).reset().text;
+    error.message = new message().set_color_blue()
+        .append(texts.IncorrectArrayInRemoveElementFromSortedObjectArray)
+        .reset().text;
+    throw error;
+}
