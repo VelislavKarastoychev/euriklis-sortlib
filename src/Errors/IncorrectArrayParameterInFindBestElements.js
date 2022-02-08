5380559c6303f42dbe3be231ab843af05a0d42d4
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-08 23:29:56 +0200

    error message for the incorrect array parameter in the find best elements in sorted array method

--- src/Errors/IncorrectArrayParameterInFindBestElements.js
+++ src/Errors/IncorrectArrayParameterInFindBestElements.js
@@ -1,11 +1,12 @@
-'use strict'
-const texts = require('./errorTexts')
-const message = require('@euriklis/message')
-module.exports = () => {
-    const error_message = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset()
-        .set_color_red().append_not_check_mark().append_white_space()
+'use strict';
+import message from '@euriklis/message';
+import * as texts from './errorTexts.js';
+export default () => {
+    const error = new Error();
+    error.name = new message().bold().italic().underline()
+        .set_color_yellow().append(texts.ErrorText).reset().text;
+    error.message = new message().set_color_red().append_not_check_mark().append_white_space()
         .set_color_cyan().append(texts.IncorrectArrayParameterInFindBestElements)
         .reset().text
-    throw new Error(error_message)
+    throw error;
 }
