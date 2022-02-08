3138ac5cdb51de1e7ff66ec82c68dfb003284159
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-08 16:27:00 +0200

    Errors of the @euriklis/sortlib library...

--- src/Errors/index.js
+++ src/Errors/index.js
@@ -1,39 +1,73 @@
 'use strict'
-module.exports = {
-    IncorrectArrayDeclarationInSortLibConstructor: require('./IncorrectArrayDeclarationInSortLibConstructor'),
-    IncorrectArrayInAddElementInSortedArray: require('./IncorrectArrayInAddElementInSortedArray'),
-    IncorrectArrayInSetterArray: require('./IncorrectArrayInSetterArray'),
-    IncorrectArgumentInIndexSetter: require('./IncorrectArgumentInIndexSetter'),
-    IncorrectStatusInSetter: require('./IncorrectStatusInSetter'),
-    IncorrectStatusParameterInBucketSort: require('./IncorrectArrayParameterInBucketSort'),
-    IncorrectParameterInGRA: require('./IncorrectParameterInGRA'),
-    IncorrectArrayParameterInFindBestElements : require('./IncorrectArrayParameterInFindBestElements'),
-    IncorrectArrayParameterInFindWortsElements : require('./IncorrectArrayParameterInFindWorstElements'),
-    IncorrectArrayParameterInSortObjectArray : require('./IncorrectArrayParameterInSortObjectArray'),
-    IncorrectPropertyInSortObjectArray : require('./IncorrectPropertyInSortObjectArray'),
-    IncorrectArrayParameterInFindBestInObjectArray : require('./IncorrectArrayParameterInFindBestInObjectArray'),
-    IncorrectPropertyParameterInFindBestInObjectArray : require('./IncorrectPropertyParameterInFindBestInObjectArray'),
-    IncorrectArrayParameterInFindWorstInObjectArray : require('./IncorrectArrayParameterInFindWorstInObjectArray'),
-    IncorrectPropertyParameterInFindWorstInObjectArray : require('./IncorrectPropertyParameterInFindWorstInObjectArray'),
-    IncorrectCountParameterInFindWorstInObjectArray : require('./IncorrectCountParameterInFindWorstInObjectArray'),
-    IncorrectArrayParameterInBucketSort : require('./IncorrectArrayParameterInBucketSort'),   
-    IncorrectElementInFindElementInSortedArray : require('./IncorrectElementInFindElementInSortedArray'),
-    IncorrectArrayParameterInFindElementInSortedArray : require('./IncorrectArrayParameterInFindElementInSortedArray'),
-    IncorrectArrayParameterInAddElementInSortedObjectArrayByProperty : require('./IncorrectArrayParameterInAddElementInSortedObjectArrayByProperty'),
-    IncorrectPropertyParameterInAddElementInSortedObjectArrayByProperty : require('./IncorrectPropertyParameterInAddElementInSortedObjectArrayByProperty'),
-    IncorrectElementInAddElementInSortedObjectArrayByProperty : require('./IncorrectElementInAddElementInSortedObjectArrayByProperty'),
-    IncorrectLengthInGRSA : require('./IncorrectLengthInGRSA'),
-    IncorrectWordSizeInGRSA : require('./IncorrectWordSizeInGRSA'),
-    IncorrectArrayInFindElementsInSortedObjectArray : require('./IncorrectArrayInFindElementsInSortedObjectArray'),
-    IncorrectPropertyParameterInFindElementsInSortedObjectArray : require('./IncorrectPropertyParameterInFindElementsInSortedObjectArray'),
-    IncorrectElementParameterInFindElementsInSortedObjectArray : require('./IncorrectElementParameterInFindElementsInSortedObjectArray'),
-    IncorrectArrayInFilterWithValidator: require('./IncorrectArrayInFilterWithValidator'),
-    IncorrectArgumentOfCallbackInFilterWithValidator: require('./IncorrectArgumentOfCallbackInFilterWithValidator'),
-    IncorrectArrayInRemoveElementFromSortedArray: require('./IncorrectArrayInRemoveElementFromSortedArray'),
-    IncorrectElementParameterInRemoveElementFromSortedArray: require('./IncorrectElementInRemoveElementFromSortedArray'),
-    IncorrectArrayInRemoveElementFromSortedObjectArray: require('./IncorrectArrayInRemoveElementFromSortedObjectArray'),
-    IncorrectPropertyParameterInRemoveElementFromSortedObjectArray: require('./IncorrectPropertyParameterInRemoveElementFromSortedObjectArray'),
-    IncorrectElementParameterInRemoveElementFromSortedObjectArray: require('./IncorrectElementParameterInRemoveElementFromSortedObjectArray'),
-    IncorrectModeParameterInRemoveElementFromSortedObjectArray: require('./IncorrectModeParameterInRemoveElementInSortedObjectArray'),
-         
-}
+import IncorrectArrayDeclarationInSortLibConstructor from './IncorrectArrayDeclarationInSortLibConstructor.js';
+import IncorrectArrayInAddElementInSortedArray from './IncorrectArrayInAddElementInSortedArray.js';
+import IncorrectArrayInSetterArray from './IncorrectArrayInSetterArray.js';
+import IncorrectArgumentInIndexSetter from './IncorrectArgumentInIndexSetter.js';
+import IncorrectStatusInSetter from './IncorrectStatusInSetter.js'; 
+import IncorrectStatusParameterInBucketSort from './IncorrectArrayParameterInBucketSort.js';
+import IncorrectParameterInGRA from './IncorrectParameterInGRA.js';
+import IncorrectArrayParameterInFindBestElements from './IncorrectArrayParameterInFindBestElements.js';
+import IncorrectArrayParameterInFindWorstElements from './IncorrectArrayParameterInFindWorstElements.js';
+import IncorrectArrayParameterInSortObjectArray from './IncorrectArrayParameterInSortObjectArray.js';
+import IncorrectPropertyInSortObjectArray from './IncorrectPropertyInSortObjectArray.js';
+import IncorrectArrayParameterInFindBestInObjectArray from './IncorrectArrayParameterInFindBestInObjectArray.js';
+import IncorrectPropertyParameterInFindBestInObjectArray from './IncorrectPropertyParameterInFindBestInObjectArray.js';
+import IncorrectArrayParameterInFindWorstInObjectArray from './IncorrectArrayParameterInFindWorstInObjectArray.js';
+import IncorrectPropertyParameterInFindWorstInObjectArray from './IncorrectPropertyParameterInFindWorstInObjectArray.js';
+import IncorrectCountParameterInFindWorstInObjectArray from './IncorrectCountParameterInFindWorstInObjectArray.js';
+import IncorrectArrayParameterInBucketSort from './IncorrectArrayParameterInBucketSort.js';
+import IncorrectElementInFindElementInSortedArray from './IncorrectElementInFindElementInSortedArray.js';
+import IncorrectArrayParameterInFindElementInSortedArray from './IncorrectArrayParameterInFindElementInSortedArray.js';
+import IncorrectArrayParameterInAddElementInSortedObjectArrayByProperty from './IncorrectArrayParameterInAddElementInSortedObjectArrayByProperty.js';
+import IncorrectPropertyParameterInAddElementInSortedObjectArrayByProperty from './IncorrectPropertyParameterInAddElementInSortedObjectArrayByProperty.js';
+import IncorrectElementInAddElementInSortedObjectArrayByProperty from './IncorrectElementInAddElementInSortedObjectArrayByProperty.js';
+import IncorrectLengthInGRSA from './IncorrectLengthInGRSA.js';
+import IncorrectWordSizeInGRSA from './IncorrectWordSizeInGRSA.js';
+import IncorrectArrayInFindElementsInSortedObjectArray from './IncorrectArrayInFindElementsInSortedObjectArray.js';
+import IncorrectPropertyParameterInFindElementsInSortedObjectArray from './IncorrectPropertyParameterInFindElementsInSortedObjectArray.js';
+import IncorrectElementParameterInFindElementsInSortedObjectArray from './IncorrectElementParameterInFindElementsInSortedObjectArray.js';
+import IncorrectArrayInFilterWithValidator from './IncorrectArrayInFilterWithValidator.js';
+import IncorrectArgumentOfCallbackInFilterWithValidator from './IncorrectArgumentOfCallbackInFilterWithValidator.js';
+import IncorrectArrayInRemoveElementFromSortedArray from './IncorrectArrayInRemoveElementFromSortedArray.js';
+import IncorrectElementParameterInRemoveElementFromSortedArray from './IncorrectElementParameterInRemoveElementFromSortedArray.js';
+import IncorrectArrayInRemoveElementFromSortedObjectArray from './IncorrectArrayInRemoveElementFromSortedObjectArray.js';
+import IncorrectPropertyParameterInRemoveElementFromSortedObjectArray from './IncorrectPropertyParameterInRemoveElementFromSortedObjectArray.js';
+import IncorrectElementParameterInRemoveElementFromSortedObjectArray from './IncorrectElementParameterInRemoveElementFromSortedObjectArray.js';
+import IncorrectModeParameterInRemoveElementFromSortedObjectArray from './IncorrectModeParameterInRemoveElementInSortedObjectArray.js';
+export {
+    IncorrectArrayDeclarationInSortLibConstructor,
+    IncorrectArrayInAddElementInSortedArray,
+    IncorrectArrayInSetterArray,
+    IncorrectArgumentInIndexSetter,
+    IncorrectStatusInSetter,
+    IncorrectStatusParameterInBucketSort,
+    IncorrectParameterInGRA,
+    IncorrectArrayParameterInFindBestElements,
+    IncorrectArrayParameterInFindWorstElements,
+    IncorrectArrayParameterInSortObjectArray,
+    IncorrectPropertyInSortObjectArray,
+    IncorrectArrayParameterInFindBestInObjectArray,
+    IncorrectPropertyParameterInFindBestInObjectArray,
+    IncorrectArrayParameterInFindWorstInObjectArray,
+    IncorrectPropertyParameterInFindWorstInObjectArray,
+    IncorrectCountParameterInFindWorstInObjectArray,
+    IncorrectArrayParameterInBucketSort,   
+    IncorrectElementInFindElementInSortedArray,
+    IncorrectArrayParameterInFindElementInSortedArray,
+    IncorrectArrayParameterInAddElementInSortedObjectArrayByProperty,
+    IncorrectPropertyParameterInAddElementInSortedObjectArrayByProperty,
+    IncorrectElementInAddElementInSortedObjectArrayByProperty,
+    IncorrectLengthInGRSA,
+    IncorrectWordSizeInGRSA,
+    IncorrectArrayInFindElementsInSortedObjectArray,
+    IncorrectPropertyParameterInFindElementsInSortedObjectArray,
+    IncorrectElementParameterInFindElementsInSortedObjectArray,
+    IncorrectArrayInFilterWithValidator,
+    IncorrectArgumentOfCallbackInFilterWithValidator,
+    IncorrectArrayInRemoveElementFromSortedArray,
+    IncorrectElementParameterInRemoveElementFromSortedArray,
+    IncorrectArrayInRemoveElementFromSortedObjectArray,
+    IncorrectPropertyParameterInRemoveElementFromSortedObjectArray,
+    IncorrectElementParameterInRemoveElementFromSortedObjectArray,
+    IncorrectModeParameterInRemoveElementFromSortedObjectArray,
+};
