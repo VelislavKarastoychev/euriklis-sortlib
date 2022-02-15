375c526f65b6e071590e2c8905e2fe484bd73cbf
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-15 19:03:08 +0200

    Incorrect array parameter for the find_worsts_elements_in_sorted_array() method of the euriklis/sortlib library.

--- src/Errors/IncorrectArrayParameterInFindWorstElements.js
+++ src/Errors/IncorrectArrayParameterInFindWorstElements.js
@@ -1,7 +1,7 @@
 'use strict'
-const message = require('@euriklis/message')
-const texts = require('./errorTexts')
-module.exports = () => {
+import message from '@euriklis/message';
+import * as texts from './errorTexts.js';
+export default () => {
     const error_message = new message().bold().italic().underline()
         .set_color_yellow().append(texts.ErrorText).reset()
         .set_color_red().append_not_check_mark().append_white_space()
