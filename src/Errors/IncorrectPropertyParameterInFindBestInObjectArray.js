d39417033175d65008302cbcab685903bbde20e5
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-15 19:30:52 +0200

    error message for the incorrect property parameter in the find_best_elements_in_sorted_object_array() method of the euriklis/sortlib

--- src/Errors/IncorrectPropertyParameterInFindBestInObjectArray.js
+++ src/Errors/IncorrectPropertyParameterInFindBestInObjectArray.js
@@ -1,10 +1,12 @@
 'use strict'
-const message = require('@euriklis/message')
-const texts = require('./errorTexts')
-module.exports = () => {
-    let error_message = new message().bold().italic().underline().set_color_yellow()
-        .append(texts.ErrorText).reset().set_color_red().append_not_check_mark()
+import message from '@euriklis/message';
+import * as texts from './errorTexts.js';
+export default () => {
+    const error = new Error(); 
+    error.name = new message().bold().italic().underline().set_color_yellow()
+        .append(texts.ErrorText).reset().text;
+    error.message = new message().set_color_red().append_not_check_mark()
         .append_white_space().set_color_cyan().append(texts.IncorrectPropertyParameterInFindBestInObjectArray)
         .reset().text
-    throw new Error(error_message)
+    throw error;
 }
