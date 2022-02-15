20243f37d1d0f65000dbd1b83b4e0180719e42d6
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-15 19:12:05 +0200

    error message for the incorrect array in the find worst method of the euriklis/sortlib library.

--- src/Errors/IncorrectArrayParameterInFindWorstElements.js
+++ src/Errors/IncorrectArrayParameterInFindWorstElements.js
@@ -2,10 +2,12 @@
 import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
 export default () => {
-    const error_message = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset()
+    const error = new Error();
+    error.name = new message().bold().italic().underline()
+        .set_color_yellow().append(texts.ErrorText).reset().text;
+    error.message = new message()
         .set_color_red().append_not_check_mark().append_white_space()
         .set_color_cyan().append(texts.IncorrectArrayParameterInFindWorstElements)
         .reset().text
-    throw new Error(error_message)
+    throw error;
 }
