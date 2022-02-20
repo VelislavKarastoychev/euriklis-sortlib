4ea13d429fe96773f74f86448c9e5130a41822ee
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-20 21:34:49 +0200

    error message for the array parameter in the bucket sort method.

--- src/Errors/IncorrectArrayParameterInBucketSort.js
+++ src/Errors/IncorrectArrayParameterInBucketSort.js
@@ -5,7 +5,7 @@ export default () => {
     const error = new Error();
     error.name = new message().bold().italic().underline()
         .set_color_yellow().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red().append_not_check_mark().append_white_space()
+    error.message = new message().set_color_red().append_warning_sign().append_white_space()
         .append(texts.IncorrectArrayParameterInBucketSort).reset().text
     throw error;
 }
