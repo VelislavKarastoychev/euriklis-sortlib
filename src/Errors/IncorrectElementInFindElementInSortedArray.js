745906a27720c3f7823d6ed36c78fe57f0cf3c86
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-20 21:38:52 +0200

    error element parameter in the find element in sorted object array method of the @euriklis/message library.

--- src/Errors/IncorrectElementInFindElementInSortedArray.js
+++ src/Errors/IncorrectElementInFindElementInSortedArray.js
@@ -1,15 +1,13 @@
-'use strict'
-const message = require('@euriklis/message');
-const texts = require('./errorTexts');
-module.exports = () => {
-    const err_name = new message().bold().italic().underline()
+'use strict';
+import message from '@euriklis/message';
+import * as texts from './errorTexts.js';
+export default () => {
+    const error = new Error(); 
+    error.name = new message().bold().italic().underline()
        .set_color_yellow().append(texts.ErrorText).reset().text;
-    const err_message = new message().set_color_red().append_not_check_mark()
+    error.message = new message().set_color_red().append_not_check_mark()
        .append_white_space().set_color_cyan()
        .append(texts.IncorrectElementInFindElementInSortedArray)
-       .reset().text
-    let error = new Error();
-    error.name = err_name;
-    error.message = err_message;
+       .reset().text;
     throw error; 
 }
