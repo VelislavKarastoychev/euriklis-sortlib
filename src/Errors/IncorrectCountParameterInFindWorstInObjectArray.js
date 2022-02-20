40ac23dcd1b62909947057a99d600618fff63c28
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-20 21:31:41 +0200

    error message for the count parameter of the find worst elements in sorted object array method of the @euriklis/message library.

--- src/Errors/IncorrectCountParameterInFindWorstInObjectArray.js
+++ src/Errors/IncorrectCountParameterInFindWorstInObjectArray.js
@@ -1,10 +1,12 @@
-'use strict'
-const message = require('@euriklis/message')
-const texts = require('./errorTexts')
+'use strict';
+import message from '@euriklis/message';
+import * as texts from './errorTexts.js';
 module.exports = () => {
-    let error_message = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset().set_color_red()
-        .append_not_check_mark().append_white_space().set_color_cyan()
+    const error = new Error();
+    error.name = new message().bold().italic().underline()
+        .set_color_yellow().append(texts.ErrorText).reset().text;
+    error.message = new message().set_color_red()
+        .append_warning_sign().append_white_space().set_color_cyan()
         .append(texts.IncorrectCountParameterInFindWorstInObjectArray).reset().text
-    throw new Error(error_message)
+    throw error;
 }
