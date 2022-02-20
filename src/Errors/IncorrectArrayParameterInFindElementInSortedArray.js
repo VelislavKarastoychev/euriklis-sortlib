992d324a0e013be480db1f5b1b876a11673c2d58
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-20 21:43:57 +0200

    error array parameter in the find element in sorted array method of the @euriklis/sortlib library.

--- src/Errors/IncorrectArrayParameterInFindElementInSortedArray.js
+++ src/Errors/IncorrectArrayParameterInFindElementInSortedArray.js
@@ -1,16 +1,14 @@
 'use strict';
-const texts = require('./errorTexts');
-const message = require('@euriklis/message');
-module.exports = () => {
-    const err_name = new message().bold().italic().underline()
+import message from '@euriklis/message';
+import * as texts from './errorTexts.js';
+export default () => {
+   const error = new Error();
+   error.name = new message().bold().italic().underline()
        .set_color_yellow().append(texts.ErrorText).reset().text;
-    const err_message = new message().bold().italic().underline()
-       .set_color_red().append_not_check_mark()
+   error.message = new message().bold().italic().underline()
+       .set_color_red().append_warning_sign()
        .append_white_space().set_color_cyan()
        .append(texts.IncorrectArrayInFindElementInSortedArray)
        .reset().text;
-    let error = new Error();
-    error.name = err_name;
-    error.message = err_message;
     throw error;
 }
