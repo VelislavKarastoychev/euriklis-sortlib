e20cd51df7ace2cef1b02c2cef30b3d7e106ab05
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-20 21:49:06 +0200

    error array in the add element in sorted object array by property method of the @euriklis/sortlib library.

--- src/Errors/IncorrectArrayParameterInAddElementInSortedObjectArrayByProperty.js
+++ src/Errors/IncorrectArrayParameterInAddElementInSortedObjectArrayByProperty.js
@@ -1,15 +1,13 @@
 'use strict';
-const texts = require('./errorTexts');
-const message = require('@euriklis/message');
-module.exports = () => {
-    const err_name = new message().bold().italic().underline()
-       .set_color_yellow().append(texts.ErrorText).reset().text;
-    const err_message = new message().set_color_red()
-       .append_not_check_mark().append_white_space().set_color_cyan()
-       .append(texts.IncorrectArrayParameterInAddElementInSortedObjectArrayByProperty)
-       .reset().text;
-    let error = new Error();
-    error.name = err_name;
-    error.message = err_message;
-    throw error;
+import message from '@euriklis/message';
+import * as texts from './errorTexts.js';
+export default () => {
+   const error = new Error();
+   error.name = new message().bold().italic().underline()
+      .set_color_yellow().append(texts.ErrorText).reset().text;
+   error.message = new message().set_color_red()
+      .append_not_check_mark().append_white_space().set_color_cyan()
+      .append(texts.IncorrectArrayParameterInAddElementInSortedObjectArrayByProperty)
+      .reset().text;
+   throw error;
 }
