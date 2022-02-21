9949118875fddf8b2f896179c4d249184e547440
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-21 21:30:07 +0200

    Implementation of the Sorting algorithms

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,6 +1,6 @@
 'use strict'
-import * as sort_algorithms from './SortingAlgorithms.js';
-import * as sort_algorithms_list from './SortAlgorithmList.js';
+import sort_algorithms from './SortingAlgorithms.js';
+import sort_algorithms_list from './SortAlgorithmList.js';
 export  {
     sort_algorithms,
     sort_algorithms_list
