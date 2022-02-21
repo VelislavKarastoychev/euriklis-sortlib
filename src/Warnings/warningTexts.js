719bb630654d4275f1fca66a8e24eeb9825ef416
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-21 21:11:46 +0200

    warning texts

--- src/Warnings/warningTexts.js
+++ src/Warnings/warningTexts.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 const WarningText = 'Euriklis SortLib package warning message:\n'
 const UndefinedArrayInSortLibConstructor = 'Undefined array in the SortLib class constructor. Note that to make sorting and to use the provided algorithms you have to declare an number or string array in the constructor options or parameters object.'
 const IncorrectAlgorithmDeclaration = 'Incorrect declaration of the algorithm parameter in the constructor parameter. Please note that the algorithm has to be a string with possible values "merge sort", "quick sort", "heap sort", "insertion sort", "selection sort", "cocktail sort", "bucket sort" and "bubble sort"'
@@ -8,7 +8,7 @@ const IncorrectCountParameterInFindBestElements = 'Incorrect type of the second 
 const IncorrectCountParameterInFindWorstElements = 'Incorrect type of the second parameter of the method find_worst_elements. Note that the parameter has to be an integer from 1 to the length of the array or a real number from 0 and 1.'
 const IncorrectOrUndefinedModeParameterInSortObjectArray = 'Incorrect or undefined mode parameter in the sort_object_array static method of the SortLib package. Note that the mode has to be only a boolean value or a string with values "decrease" or "increase".'
 const IncorrectOrUndefinedAlgorithmParameterInSortObjectArray = 'Incorrect or undefined algorithm parameter in the sort_object_array_by_property static method. Note that this parameter has to be a string with the values "quick sort", "merge sort", "heap sort" and "bucket sort".'
-module.exports = {
+export {
     WarningText,
     UndefinedArrayInSortLibConstructor,
     IncorrectAlgorithmDeclaration,
@@ -18,5 +18,4 @@ module.exports = {
     IncorrectCountParameterInFindWorstElements,
     IncorrectOrUndefinedModeParameterInSortObjectArray,
     IncorrectOrUndefinedAlgorithmParameterInSortObjectArray,
-
-}
+};
