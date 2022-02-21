3f8967d0a38a855877b84ef0752ddf54c419b5dd
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-21 14:51:43 +0200

    error message for the status parameter in the status setter.

--- src/Errors/IncorrectStatusInSetter.js
+++ src/Errors/IncorrectStatusInSetter.js
@@ -1,10 +1,12 @@
 'use strict';
 import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-module.exports = () => {
-    const error_message = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset().set_color_red()
+export default () => {
+    const error = new Error(); 
+    error.name = new message().bold().italic().underline()
+        .set_color_yellow().append(texts.ErrorText).reset().text
+    error.message = new message().set_color_red()
         .append_not_check_mark().set_color_cyan().append_white_space()
         .append(texts.IncorrectStatusInSetter).reset().text
-    throw new Error(error_message)
+    throw error; 
 }
