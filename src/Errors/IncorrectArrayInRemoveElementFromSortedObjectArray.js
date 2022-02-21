49c2624d5a116520287197692c9be73e64c12015
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-21 14:27:46 +0200

    error message for the array parameter in the remove_element_from_sorted_object_array() method of the @euriklis/sortlib library

--- src/Errors/IncorrectArrayInRemoveElementFromSortedObjectArray.js
+++ src/Errors/IncorrectArrayInRemoveElementFromSortedObjectArray.js
@@ -1,7 +1,7 @@
 'use strict';
-const texts = require('./errorTexts');
-const message = require('@euriklis/message');
-module.exports = () => {
+import message from '@euriklis/message';
+import * as texts from './errorTExts.js';
+export default () => {
     const error = new Error();
     error.name = new message().bold().italic()
         .underline().set_color_yellow().append(texts.ErrorText).reset().text;
