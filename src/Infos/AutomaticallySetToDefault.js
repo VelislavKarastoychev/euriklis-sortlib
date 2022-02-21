10e59be678a1d98113190d39876ea0aec949a821
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-21 16:07:50 +0200

    Executable information message

--- src/Infos/AutomaticallySetToDefault.js
+++ src/Infos/AutomaticallySetToDefault.js
@@ -1,10 +1,10 @@
-'use strict'
-const message = require('@euriklis/message')
-const texts = require('./infoTexts')
-module.exports = (parameters) => {
+'use strict';
+import message from '@euriklis/message';
+import * as texts from './infoTexts.js';
+export default (parameters) => {
     return new message().bold().italic().underline()
         .set_color_yellow().append(texts.InfoText).reset()
         .set_color_green().append_check_mark().append_white_space()
         .reset().set_color_cyan().append(texts.AutomaticallySetToDefault(parameters))
-        .reset().log()
-}
+        .reset().log();
+};
