5f5117aaabb1f8d97fd03feb486345acbafe09d9
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-21 21:10:38 +0200

    incorrect declaration of the algorithm parameter in the sort_object_array_by_property() method

--- src/Warnings/IncorrectOrUndefinedAlgorithmParameterInSortObjectArray.js
+++ src/Warnings/IncorrectOrUndefinedAlgorithmParameterInSortObjectArray.js
@@ -1,9 +1,9 @@
-'use strict'
-const message = require('@euriklis/message')
-const texts = require('./warningTexts')
-module.exports = () => {
+'use strict';
+import message from '@euriklis/message';
+import * as texts from './warningTexts.js';
+export default () => {
     return new message().bold().italic().underline().set_color_yellow()
         .append(texts.WarningText).reset().set_color_red().append_not_check_mark()
         .append_white_space().set_color_cyan().append(texts.IncorrectOrUndefinedAlgorithmParameterInSortObjectArray)
-        .reset().log()
-}
+        .reset().log();
+};
