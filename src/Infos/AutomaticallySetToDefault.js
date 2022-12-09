c357945ee6989f114c9160b8c697213358e42714
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:13:08 +0200

    information message for the setting to the default of some parameter.

--- src/Infos/AutomaticallySetToDefault.js
+++ src/Infos/AutomaticallySetToDefault.js
@@ -1,10 +1,7 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './infoTexts.js';
-export default (parameters) => {
-    return new message().bold().italic().underline()
-        .set_color_yellow().append(texts.InfoText).reset()
-        .set_color_green().append_check_mark().append_white_space()
-        .reset().set_color_cyan().append(texts.AutomaticallySetToDefault(parameters))
-        .reset().log();
+const AutomaticallySetToDefault = (parameters) => {
+    console.log(texts.InfoText);
+    console.log(texts.AutomaticallySetToDefault(parameters))
 };
+export default AutomaticallySetToDefault;
