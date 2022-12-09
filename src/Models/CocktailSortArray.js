ee0d6f7b08dd530f009885abf5b0f7d06c182be3
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 22:25:26 +0200

    Implementation of the cocktail sort array.

---
+++ src/Models/CocktailSortArray.js
@@ -0,0 +1,62 @@
+'use strict';
+/**
+ * 
+ * @param {Array.<number | string>} array 
+ * @param {boolean | 'increase' | 'decrease'} mode
+ * @returns {Array.<number | string>}
+ * @description this utility function implements
+ * the cocktail sorting algorithm that is a variant
+ * of the bubble sort algorithm. Note that this algorithm
+ * is not fast (has complexity proportional to O(n^2)).
+ */
+function cocktail_sort(array, mode) {
+    let n = array.length, sorted_array = [];
+    let i, j, start, end, condition, temp, swapped = true
+    if (typeof mode === 'undefined') mode = true
+    if (mode === 'decrease') mode = false
+    for (i = 0; i < n >> 2; i++) {
+        j = i << 2;
+        sorted_array[j] = array[j];
+        sorted_array[j + 1] = array[j + 1];
+        sorted_array[j + 2] = array[j + 2];
+        sorted_array[j + 3] = array[j + 3];
+    }
+    if (n % 4 >= 3) {
+        sorted_array[n - 3] = array[n - 3];
+    }
+    if (n % 4 >= 2) {
+        sorted_array[n - 2] = array[n - 2];
+    }
+    if (n % 4 >= 1) {
+        sorted_array[n - 1] = array[n - 1];
+    }
+    start = 0
+    end = n - 1
+    while (swapped) {
+        swapped = false
+        for (i = start; i < end; i++) {
+            condition = mode ? sorted_array[i] > sorted_array[i + 1] : sorted_array[i] < sorted_array[i + 1]
+            if (condition) {
+                temp = sorted_array[i]
+                sorted_array[i] = sorted_array[i + 1]
+                sorted_array[i + 1] = temp
+                swapped = true
+            }
+        }
+        if (!swapped) break
+        swapped = false
+        --end
+        for (i = end - 1; i > start - 1; i--) {
+            condition = mode ? sorted_array[i] > sorted_array[i + 1] : sorted_array[i] < sorted_array[i + 1]
+            if (condition) {
+                temp = sorted_array[i]
+                sorted_array[i] = sorted_array[i + 1]
+                sorted_array[i + 1] = temp
+                swapped = true
+            }
+        }
+        ++start
+    }
+    return sorted_array
+}
+export default cocktail_sort;
