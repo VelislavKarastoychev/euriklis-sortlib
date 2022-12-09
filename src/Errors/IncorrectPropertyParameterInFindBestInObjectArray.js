b6a89f2c193108a193e5b27be3235d508b5e83f6
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:21:53 +0200

    error message for incorrect property for the find best elements for sorted object array.

--- src/Errors/IncorrectPropertyParameterInFindBestInObjectArray.js
+++ src/Errors/IncorrectPropertyParameterInFindBestInObjectArray.js
@@ -1,12 +1,9 @@
 'use strict'
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
-    const error = new Error(); 
-    error.name = new message().bold().italic().underline().set_color_yellow()
-        .append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red().append_not_check_mark()
-        .append_white_space().set_color_cyan().append(texts.IncorrectPropertyParameterInFindBestInObjectArray)
-        .reset().text
+const IncorrectPropertyParameterInFindBestInSortedObjectArray = () => {
+    const error = new Error();
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectPropertyParameterInFindBestInObjectArray;
     throw error;
 }
+export default IncorrectPropertyParameterInFindBestInSortedObjectArray;
