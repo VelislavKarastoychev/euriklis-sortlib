34241d3cf678e75bec113347a0491d6190512c39
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:50:48 +0200

    error message for incorrect array in the filter with validator message.

--- src/Errors/IncorrectArrayInFilterWithValidator.js
+++ src/Errors/IncorrectArrayInFilterWithValidator.js
@@ -1,12 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectArrayInFilterWithValidator = () => {
     const error = new Error();
-    error.name = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red()
-        .append_not_check_mark().append_white_space()
-        .set_color_blue().append(texts.IncorrectArrayInFilterWithValidator).reset().text;
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectArrayInFilterWithValidator;
     throw error;
 }
+export default IncorrectArrayInFilterWithValidator;
