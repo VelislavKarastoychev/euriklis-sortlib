2b2751639a25ae12b1887de6d146548cd88ea721
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:34:08 +0200

    error message for the count parameter in find worst in sorted object array.

--- src/Errors/IncorrectCountParameterInFindWorstInObjectArray.js
+++ src/Errors/IncorrectCountParameterInFindWorstInObjectArray.js
@@ -1,12 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectCountParameterInFindWorstInObjectArray = () => {
     const error = new Error();
-    error.name = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red()
-        .append_warning_sign().append_white_space().set_color_cyan()
-        .append(texts.IncorrectCountParameterInFindWorstInObjectArray).reset().text
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectCountParameterInFindWorstInObjectArray
     throw error;
 }
+export default IncorrectCountParameterInFindWorstInObjectArray;
