fe3c0aa64f63221377358554a19bca7c36b0495a
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:49:54 +0200

    Incorrect array property in find elements in sorted object array method.

--- src/Errors/IncorrectArrayInFindElementsInSortedObjectArray.js
+++ src/Errors/IncorrectArrayInFindElementsInSortedObjectArray.js
@@ -1,13 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectArrayInFindElementsInSortedObjectArray = () => {
     const error = new Error();
-    error.name = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText)
-        .reset().text;
-    error.message = new message().set_color_red().append_not_check_mark()
-        .append_white_space().set_color_blue()
-        .append(texts.IncorrectArrayInFindElementsInSortedObjectArray).reset().text;
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectArrayInFindElementsInSortedObjectArray;
     throw error;
 }
+export default IncorrectArrayInFindElementsInSortedObjectArray;
