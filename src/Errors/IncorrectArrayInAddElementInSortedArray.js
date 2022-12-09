f3ce527af34e8bc8c381e16417beaf0c977a92c4
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:51:41 +0200

    error message for incorrect array in add element in sorted array.

--- src/Errors/IncorrectArrayInAddElementInSortedArray.js
+++ src/Errors/IncorrectArrayInAddElementInSortedArray.js
@@ -1,12 +1,9 @@
 'use strict'
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectArrayInAddElementInSortedArray = () => {
     const error = new Error();
-    error.name = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red()
-        .append_warning_sign().set_color_cyan().append_white_space()
-        .append(texts.IncorrectArrayDeclarationInSortLibConstructor).text
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectArrayDeclarationInSortLibConstructor;
     throw error;
 }
+export default IncorrectArrayInAddElementInSortedArray;
