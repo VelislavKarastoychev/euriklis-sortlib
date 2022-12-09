88bff014608f65506050fee8368eb70df4a05bec
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:39:16 +0200

    error message for the array parameter in the find element in sorted array

--- src/Errors/IncorrectArrayParameterInFindElementInSortedArray.js
+++ src/Errors/IncorrectArrayParameterInFindElementInSortedArray.js
@@ -1,14 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
-   const error = new Error();
-   error.name = new message().bold().italic().underline()
-       .set_color_yellow().append(texts.ErrorText).reset().text;
-   error.message = new message().bold().italic().underline()
-       .set_color_red().append_warning_sign()
-       .append_white_space().set_color_cyan()
-       .append(texts.IncorrectArrayInFindElementInSortedArray)
-       .reset().text;
+const IncorrectArrayParameterInFindElementInSortedArray = () => {
+    const error = new Error();
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectArrayInFindElementInSortedArray
     throw error;
 }
+export default IncorrectArrayParameterInFindElementInSortedArray;
