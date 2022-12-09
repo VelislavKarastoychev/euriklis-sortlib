808ac9ab2172734daf48e807271219adebd97c6d
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:43:15 +0200

    error message for the array parameter in the bucket sort algorithm.

--- src/Errors/IncorrectArrayParameterInBucketSort.js
+++ src/Errors/IncorrectArrayParameterInBucketSort.js
@@ -1,11 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectArrayParameterInBucketSort = () => {
     const error = new Error();
-    error.name = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red().append_warning_sign().append_white_space()
-        .append(texts.IncorrectArrayParameterInBucketSort).reset().text
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectArrayParameterInBucketSort;
     throw error;
 }
+export default IncorrectArrayParameterInBucketSort;
