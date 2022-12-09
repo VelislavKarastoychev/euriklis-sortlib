d5eeac27e5da2610c462062cefdd06133066fb17
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:10:07 +0200

    warning message for when the algorithm is incorrectly declared.

--- src/Warnings/IncorrectAlgorithmDeclaration.js
+++ src/Warnings/IncorrectAlgorithmDeclaration.js
@@ -1,10 +1,7 @@
 'use strict'
-import message from '@euriklis/message';
 import * as texts from './warningTexts.js';
-export default () => {
-    return new message().bold().italic().underline()
-        .set_color_yellow().append(texts.WarningText).reset()
-        .set_color_red().append_not_check_mark().reset()
-        .append_white_space().set_color_cyan().append(texts.IncorrectAlgorithmDeclaration)
-        .reset().log();
+const IncorrectAlgorithmDeclaration = () => {
+    console.log(texts.WarningText)
+    console.log(texts.IncorrectAlgorithmDeclaration)
 };
+export default IncorrectAlgorithmDeclaration;
