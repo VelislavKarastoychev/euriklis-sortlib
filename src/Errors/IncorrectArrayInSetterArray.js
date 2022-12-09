eef6015076c8d2f05bee2a74e2ef30e94e561712
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:45:19 +0200

    error message for the array parameter in the setter array method.

--- src/Errors/IncorrectArrayInSetterArray.js
+++ src/Errors/IncorrectArrayInSetterArray.js
@@ -1,12 +1,9 @@
 'use strict'
-import message from '@euriklis/message';
 import * as  texts from './errorTexts.js';
-export default () => {
+const IncorrectArrayInSetterArray = () => {
     const error = new Error();
-    error.name = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red()
-        .append_warning_sign().set_color_cyan().append_white_space()
-        .append(texts.IncorrectArrayInSetterArray).reset().text;
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectArrayInSetterArray;
     throw new error;
 }
+export default IncorrectArrayInSetterArray;
