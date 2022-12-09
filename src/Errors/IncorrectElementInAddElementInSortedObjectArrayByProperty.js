942e62b6fb40704e06001487fe7210df714ef18f
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:33:03 +0200

    error message for the incorrect element for add element in sorted object array.

--- src/Errors/IncorrectElementInAddElementInSortedObjectArrayByProperty.js
+++ src/Errors/IncorrectElementInAddElementInSortedObjectArrayByProperty.js
@@ -1,13 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectElementInAddElementInSortedObjectArrayByProperty = () => {
    const error = new Error();
-   error.name = new message().bold().italic().underline()
-      .set_color_yellow().append(texts.ErrorText).reset().text;
-   error.message = new message().set_color_red().append_warning_sign()
-      .append_white_space().set_color_cyan()
-      .append(texts.IncorrectElementInAddElementInSortedObjectArrayByProperty)
-      .reset().text;
+   error.name = texts.ErrorText;
+   error.message = texts.IncorrectElementInAddElementInSortedObjectArrayByProperty
    throw error;
 }
+export default IncorrectElementInAddElementInSortedObjectArrayByProperty;
