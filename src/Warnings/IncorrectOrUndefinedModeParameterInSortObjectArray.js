debf3b3a362842a4bb1257c46f61beebbc65370b
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:04:21 +0200

    warning messsage for mode parameter for sort object array.

--- src/Warnings/IncorrectOrUndefinedModeParameterInSortObjectArray.js
+++ src/Warnings/IncorrectOrUndefinedModeParameterInSortObjectArray.js
@@ -1,9 +1,7 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './warningTexts.js';
-export default () => {
-    return new message().bold().italic().underline().set_color_yellow()
-        .append(texts.WarningText).reset().set_color_red().append_not_check_mark()
-        .append_white_space().set_color_cyan().append(texts.IncorrectOrUndefinedModeParameterInSortObjectArray)
-        .reset().log();
+const IncorrectOrUndefinedModeParameterInSortObjectArray = () => {
+    console.log(texts.WarningText)
+    console.log(texts.IncorrectOrUndefinedModeParameterInSortObjectArray)
 };
+export default IncorrectOrUndefinedModeParameterInSortObjectArray;
