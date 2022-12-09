d8155c58746720e709118b75779621aafc346e7d
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:29:39 +0200

    error message for the element parameter in the remove element form sorted array.

--- src/Errors/IncorrectElementParameterInRemoveElementFromSortedArray.js
+++ src/Errors/IncorrectElementParameterInRemoveElementFromSortedArray.js
@@ -1,12 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectElementParameterInRemoveElementFromSortedArray = () => {
     const error = new Error();
-    error.name = new message().set_color_yellow().underline().italic()
-        .bold().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red()
-        .append_warning_sign().append_white_space().set_color_blue()
-        .append(texts.IncorrectElementParameterInRemoveElementFromSortedArray).reset().text;
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectElementParameterInRemoveElementFromSortedArray;
     throw error;
 }
+export default IncorrectElementParameterInRemoveElementFromSortedArray;
