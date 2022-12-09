8f5c48ad96a00d6d65e1528b9ef21bb68bfec22e
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:31:51 +0200

    error message for incorrect element in the find elements in sorted array.

--- src/Errors/IncorrectElementInFindElementInSortedArray.js
+++ src/Errors/IncorrectElementInFindElementInSortedArray.js
@@ -1,13 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectElementInFindElementInSortedArray = () => {
     const error = new Error(); 
-    error.name = new message().bold().italic().underline()
-       .set_color_yellow().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red().append_not_check_mark()
-       .append_white_space().set_color_cyan()
-       .append(texts.IncorrectElementInFindElementInSortedArray)
-       .reset().text;
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectElementInFindElementInSortedArray
     throw error; 
 }
+export default IncorrectElementInFindElementInSortedArray;
