99b5ade7b92497da442c3939c557ec9ec9c4c474
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:47:14 +0200

    error message for the array parameter in the remove element from sorted object array by some property.

--- src/Errors/IncorrectArrayInRemoveElementFromSortedObjectArray.js
+++ src/Errors/IncorrectArrayInRemoveElementFromSortedObjectArray.js
@@ -1,12 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectArrayInRemoveElementFromSortedObjectArray = () => {
     const error = new Error();
-    error.name = new message().bold().italic()
-        .underline().set_color_yellow().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_blue()
-        .append(texts.IncorrectArrayInRemoveElementFromSortedObjectArray)
-        .reset().text;
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectArrayInRemoveElementFromSortedObjectArray
     throw error;
 }
+export default IncorrectArrayInRemoveElementFromSortedObjectArray;
