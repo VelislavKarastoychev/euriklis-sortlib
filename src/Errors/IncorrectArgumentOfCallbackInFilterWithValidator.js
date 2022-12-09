e9daa890df5ef52044bdf0305d1c8d442e1a1088
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:54:01 +0200

    error message for the argument of the callback in filter with validator.

--- src/Errors/IncorrectArgumentOfCallbackInFilterWithValidator.js
+++ src/Errors/IncorrectArgumentOfCallbackInFilterWithValidator.js
@@ -1,12 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectArgumentOfCallbackInFilterWithValidator = () => {
     const error = new Error();
-    error.name = new message().bold().italic().underscore()
-        .set_color_yellow().append(texts.ErrorText).reset().text;
-    error.message = new message().set_color_red().append_not_check_mark()
-        .append_white_space().set_color_blue().append(texts.IncorrectArgumentOfCallbackInFilterWithValidator)
-        .reset().text;
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectArgumentOfCallbackInFilterWithValidator;
     throw error;
 }
+export default IncorrectArgumentOfCallbackInFilterWithValidator;
