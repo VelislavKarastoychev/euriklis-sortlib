b2809fc342777140db775b8eaa55f8abe6d1ebf7
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:15:35 +0200

    error message for the incorrect status property.

--- src/Errors/IncorrectStatusInSetter.js
+++ src/Errors/IncorrectStatusInSetter.js
@@ -1,12 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectStatusInSetter = () => {
     const error = new Error(); 
-    error.name = new message().bold().italic().underline()
-        .set_color_yellow().append(texts.ErrorText).reset().text
-    error.message = new message().set_color_red()
-        .append_not_check_mark().set_color_cyan().append_white_space()
-        .append(texts.IncorrectStatusInSetter).reset().text
+    error.name = texts.ErrorText;
+    error.message = texts.IncorrectStatusInSetter
     throw error; 
 }
+export default IncorrectStatusInSetter;
