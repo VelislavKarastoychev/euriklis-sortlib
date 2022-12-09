c486e2d8a2d3c055c1c452b378a1f96c15b5d51a
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:23:12 +0200

    error message for incorrect property.

--- src/Errors/IncorrectPropertyParameterInAddElementInSortedObjectArrayByProperty.js
+++ src/Errors/IncorrectPropertyParameterInAddElementInSortedObjectArrayByProperty.js
@@ -1,13 +1,9 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './errorTexts.js';
-export default () => {
+const IncorrectPropertyParameterInAddElementInSortedObjectArray = () => {
    const error = new Error();
-   error.name = new message().bold().italic().underline()
-      .set_color_yellow().append(texts.ErrorText).reset().text;
-   error.message = new message().set_color_red().append_not_check_mark()
-      .append_white_space().set_color_cyan()
-      .append(texts.IncorrectPropertyParameterInAddElementInSortedObjectArrayByProperty)
-      .reset().text;
+   error.name = texts.ErrorText;
+   error.message = texts.IncorrectPropertyParameterInAddElementInSortedObjectArrayByProperty
    throw error;
 }
+export default IncorrectPropertyParameterInAddElementInSortedObjectArray;
