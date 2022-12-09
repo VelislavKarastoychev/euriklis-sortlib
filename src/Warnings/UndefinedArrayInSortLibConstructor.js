9e78248f71dc0c47cd8d2ade8b16c229a71fcfde
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:02:28 +0200

    undefined array in the SortLib package.

--- src/Warnings/UndefinedArrayInSortLibConstructor.js
+++ src/Warnings/UndefinedArrayInSortLibConstructor.js
@@ -1,9 +1,7 @@
 'use strict';
-import message from '@euriklis/message';
 import * as texts from './warningTexts.js';
-export default () => {
-    return new message().bold().italic().underline()
-        .set_color_yellow().append(texts.WarningText)
-        .reset().set_color_cyan().append(texts.UndefinedArrayInSortLibConstructor)
-        .reset().log();
+const UndefinedArrayInSortLibConstructor =  () => {
+    console.log(texts.WarningText)
+    console.log(texts.UndefinedArrayInSortLibConstructor);
 };
+export default UndefinedArrayInSortLibConstructor;
