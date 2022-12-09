c526d50acadfd643e207009eac1dbe59d58b5f0f
VelislavKarastoychev <exel_mmm@abv.bg>
2022-12-09 23:01:17 +0200

    Collection of all the Warnings message.

--- src/Warnings/index.js
+++ src/Warnings/index.js
@@ -1,19 +1,9 @@
 'use strict';
-import UndefinedArrayInSortLibConstructor from './UndefinedArrayInSortLibConstructor.js';
-import IncorrectAlgorithmDeclaration from './IncorrectAlgorithmDeclaration.js';
-import IncorrectSortModeDeclaration from './IncorrectSortModeDeclaration.js';
-import IncorrectAlgorithmInAlgorithmSetter from './IncorrectAlgorithmInAlgorithmSetter.js';
-import IncorrectCountParameterInFindBestElements from './IncorrectCountParameterInFindBestElements.js';
-import IncorrectCountParameterInFindWorstElements from './IncorrectCountParameterInFindWorstElements.js';
-import IncorrectOrUndefinedModeParameterInSortObjectArray from './IncorrectOrUndefinedModeParameterInSortObjectArray.js';
-import IncorrectOrUndefinedAlgorithmParameterInSortObjectArray from './IncorrectOrUndefinedAlgorithmParameterInSortObjectArray.js';
-export {
-    UndefinedArrayInSortLibConstructor,
-    IncorrectAlgorithmDeclaration,
-    IncorrectSortModeDeclaration,
-    IncorrectAlgorithmInAlgorithmSetter,
-    IncorrectCountParameterInFindBestElements,
-    IncorrectCountParameterInFindWorstElements,
-    IncorrectOrUndefinedModeParameterInSortObjectArray,
-    IncorrectOrUndefinedAlgorithmParameterInSortObjectArray,   
-};
+export { default as UndefinedArrayInSortLibConstructor } from './UndefinedArrayInSortLibConstructor.js';
+export { default as IncorrectAlgorithmDeclaration } from './IncorrectAlgorithmDeclaration.js';
+export { default as IncorrectSortModeDeclaration } from './IncorrectSortModeDeclaration.js';
+export { default as IncorrectAlgorithmInAlgorithmSetter } from './IncorrectAlgorithmInAlgorithmSetter.js';
+export { default as IncorrectCountParameterInFindBestElements } from './IncorrectCountParameterInFindBestElements.js';
+export { default as IncorrectCountParameterInFindWorstElements } from './IncorrectCountParameterInFindWorstElements.js';
+export { default as IncorrectOrUndefinedModeParameterInSortObjectArray } from './IncorrectOrUndefinedModeParameterInSortObjectArray.js';
+export { default as IncorrectOrUndefinedAlgorithmParameterInSortObjectArray } from './IncorrectOrUndefinedAlgorithmParameterInSortObjectArray.js';
